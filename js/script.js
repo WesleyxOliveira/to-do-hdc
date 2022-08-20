b91b04e4e83059ff7dfb2b85e3ec477e98ecf8cf
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-08-19 21:46:52 -0300

    atualizei o javascript do projeto

--- js/script.js
+++ js/script.js
@@ -7,6 +7,35 @@ const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // Funções
+const saveTodo = (text) => {
+
+    const todo = document.createElement("div");
+    todo.classList.add("todo") //adicionou uma classe a div todo.
+
+    const todoTitle = document.createElement("h3"); //criando o h3 da div
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+
+    const deleBtn = document.createElement("button");
+    deleBtn.classList.add("remove-todo");
+    deleBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleBtn);
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+    todoInput.focus();
+}
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
@@ -15,5 +44,9 @@ todoForm.addEventListener("submit", (e) => {
 
     const inputValue = todoInput.value;
 
-    console.log(inputValue);
+    if(inputValue) {
+        saveTodo(inputValue);
+    } else {
+        window.alert("Informe a tarefa que deseja adicionar!");
+    }
 })
